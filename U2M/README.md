9ec139305bfd2037e330df167843e0ea4993ee27
Scott Donohoo <scott.donohoo@databricks.com>
2024-03-29 15:56:17 -0400

    Initial U2M example

---
+++ U2M/README.md
@@ -0,0 +1,102 @@
+# Querying a Model Serving Endpoint using U2M OAuth Authentication
+
+These steps walk through the configuration needed for an application to query a Databricks model serving endpoint using U2M OAuth for authentication instead of a personal access token.
+
+1. Create a Microsoft Entra ID App Registration
+    Create an App registration for your application in Microsoft Entra ID.
+    This manages the Entra ID login for your users to the application itself.
+
+2. Install the [databricks cli](https://learn.microsoft.com/en-us/azure/databricks/dev-tools/cli/install) and the [databricks python sdk](https://docs.databricks.com/en/dev-tools/sdk-python.html#get-started-with-the-databricks-sdk-for-python) in your Python environment. Be certain to install at least version 0.21.0.
+
+3. Create an Azure Databricks Account authentication profile. Choose a Databricks profile name (ex: `AzureAcct`). You will be prompted for your Azure Databricks account ID which you can obtain from your account console at [account.azuredatabricks.com](https://account.azuredatabricks.com).
+
+
+    ```bash
+    > databricks auth login https://accounts.azuredatabricks.net
+    Databricks Profile Name: AzureAcct
+    Databricks Account ID: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+    Profile AzureAcct was successfully saved
+    ```
+
+4. Create an Azure Databricks OAuth Application Registration.  
+
+    On Azure, this is done using the databricks CLI. Parameters needed:  
+    * Name of the application to register. In this case, `flask_app_with_oauth`  
+    * Redirect URL for your app that the OAuth server redirect back to. In our sample app, this is `http://localhost:5001/callback`. If you change the redirect url in you app, make sure this url matches.  
+    * The list of OAuth scopes required for your application to access Databricks APIs. For model serving API access, `all-apis` is required. For endpoints on Azure Databricks, scopes `offline_access` and `user_impersonation` are required.
+
+    ```shell
+    > databricks account custom-app-integration create --confidential \
+    > --json '{"name":"flask_app_with_oauth", "redirect_urls":[http://localhost:5001/callback], "scopes":["all-apis","offline_access","user_impersonation"]}' –-debug
+    ```
+
+    The output should be similar to the following:
+
+    ```
+    16:20:18  INFO start pid=6839 version=0.215.0 args="databricks, account, custom-app-integration, create, --confidential, --json, {\"name\":\"flask_app_with_oauth\", \"redirect_urls\":[\http://localhost:5001/callback\], \"scopes\":[\"all-apis\",\"offline_access\",\"user_impersonation\"]}, --debug"
+    16:20:18 DEBUG Loading DEFAULT profile from /Users/scott.donohoo/.databrickscfg pid=6839 sdk=true
+    16:20:18  INFO Ignoring pat auth, because databricks-cli is preferred pid=6839 sdk=true
+    16:20:18  INFO Ignoring basic auth, because databricks-cli is preferred pid=6839 sdk=true
+    16:20:18  INFO Ignoring oauth-m2m auth, because databricks-cli is preferred pid=6839 sdk=true
+    16:20:18  INFO Refreshed OAuth token from Databricks CLI, expires on 2024-03-12 17:09:32.492687 -0400 EDT pid=6839 sdk=true
+    16:20:18 DEBUG Using Databricks CLI authentication with Databricks OAuth tokens pid=6839 sdk=true
+    16:20:18  INFO Refreshed OAuth token from Databricks CLI, expires on 2024-03-12 17:09:32.492687 -0400 EDT pid=6839 sdk=true
+    16:20:18 DEBUG POST /api/2.0/accounts/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/oauth2/custom-app-integrations
+    > {
+    >   "confidential": true,
+    >   "name": "flask_app_with_oauth",
+    >   "redirect_urls": [
+    >     "http://localhost:5001/callback"
+    >   ],
+    >   "scopes": [
+    >     "all-apis",
+    >     "offline_access",
+    >     "user_impersonation"
+    >   ]
+    > }
+    < HTTP/2.0 200 OK
+    < {
+    <   "client_id": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+    <   "client_secret": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    <   "integration_id": "af59f1a9-37e2-4e2c-a86a-fb635ed7ecd7"
+    < } pid=6839 sdk=true
+    {
+    "client_id":"xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
+    "client_secret":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "integration_id":"af59f1a9-37e2-4e2c-a86a-fb635ed7ecd7"
+    }
+    16:20:18  INFO completed execution pid=6839 exit_code=0
+    ```
+
+    Make a note of the client_id and secret_id. Your application will need this information to request an OAuth token. The integration_id is needed only to make changes to or delete the application registration.
+
+5. It is recommended to install and deploy the Quickstart scenario from the [Databricks RAG Chatbot solution accelerator](https://www.databricks.com/resources/demos/tutorials/data-science-and-ai/lakehouse-ai-deploy-your-llm-chatbot). This will create a model serving endpoint that the example client application is already set up to query. You may deploy the advanced scenario as well but the query schema will be different from the sample query in the application.  
+
+6. Edit flask_app_with_oauth.py. This sample client app is an  adapted from https://github.com/databricks/databricks-sdk-py/blob/main/examples/flask_app_with_oauth.py to call a Databricks model serving endpoint.  
+
+    Edit `DATABRICKS_INSTANCE` and `ENDPOINT_NAME` to match the Databricks workspace host and model serving endpoint in your RAG demo deployment.
+
+    If you deployed the advanced scenario, also edit the JSON query payload, ‘data’, in the call_model_serving_endpoint() function to match its schema. You can find an example query json to copy/paste when you test query the endpoint in the Databricks serving UI.
+
+
+7. Run the flask_app_with_oauth.py application using your databricks workspace url and the client_id and secret_id you collected from the output of the oauth app registration:
+
+    ```bash
+    > python flask_app_with_oauth.py https://<workspace-url>.azuredatabricks.net/ --client_id xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx  --client_secret xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+    ```
+
+    Output should similar to:
+
+    ```
+    * Serving Flask app 'flask_app_with_oauth'
+    * Debug mode: on
+    2024-03-12 17:07:14,528 [werkzeug][INFO] WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+    * Running on http://localhost:5001
+    2024-03-12 17:07:14,528 [werkzeug][INFO] Press CTRL+C to quit
+    2024-03-12 17:07:14,528 [werkzeug][INFO]  * Restarting with stat
+    2024-03-12 17:07:14,988 [werkzeug][WARNING]  * Debugger is active!
+    2024-03-12 17:07:15,000 [werkzeug][INFO]  * Debugger PIN: 636-786-142
+    ```
+ 
+
+8. In your browser, visit http://localhost:5001 . You should be prompted to authenticate with Azure, then the application will authenticate to Databricks and call the endpoint.
---
+++ U2M/flask_app_with_oauth.py
@@ -0,0 +1,202 @@
+#!env python3
+""" Flask with OAuth:
+
+This application provides end-to-end demonstration of Databricks SDK for Python
+capabilities of OAuth Authorization Code flow with PKCE security enabled. This
+can help you build a hosted app with every user using their own identity to
+access Databricks resources.
+
+If you have already Custom App:
+
+./flask_app_with_oauth.py <databricks workspace url> \
+    --client_id <app-client-id> \
+    --client_secret <app-secret> \
+    --port 5001
+
+If you want this script to register Custom App and redirect URL for you:
+
+./flask_app_with_oauth.py <databricks workspace url>
+
+You'll get prompted for Databricks Account username and password for
+script to enroll your account into OAuth and create a custom app with
+http://localhost:5001/callback as the redirect callback. Client and
+secret credentials for this OAuth app will be printed to the console,
+so that you could resume testing this app at a later stage.
+
+Once started, please open http://localhost:5001 in your browser and
+go through SSO flow to execute your test query against the model
+serving endpoint.
+"""
+
+import argparse
+import logging
+import sys
+import requests
+
+from databricks.sdk.oauth import OAuthClient
+
+APP_NAME = "flask_app_with_oauth"
+DATABRICKS_INSTANCE = 'xxxxxxxxx.azuredatabricks.net'  # Set your Databricks instance host here
+ENDPOINT_NAME = 'dbdemos_endpoint_main_rag_chatbot' # Change to the name of your model serving endpoint
+
+
+def call_model_serving_endpoint(access_token):
+    headers = {
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json'  # Explicitly set Content-Type to application/json
+    }
+
+    # Example API call to the model serving endpoint
+    api_url = f"https://{DATABRICKS_INSTANCE}/serving-endpoints/{ENDPOINT_NAME}/invocations"
+
+    # Note: endpoint query schema matches quickstart deployment scenario in the demo at
+    # https://www.databricks.com/resources/demos/tutorials/data-science-and-ai/lakehouse-ai-deploy-your-llm-chatbot
+    data = {
+        "dataframe_split": {
+            "columns": [
+                "query"
+            ],
+            "data": [
+                [
+                    "How can I track billing usage on my workspaces?"
+                ]
+            ]
+        }
+    }
+    response = requests.post(api_url, json=data, headers=headers, timeout=100)
+
+    if response.status_code != 200:
+        raise Exception(f'Request failed with status {response.status_code}, {response.text}')
+    
+    return response.json()
+
+
+def create_flask_app(oauth_client: OAuthClient, port: int):
+    """The create_flask_app function creates a Flask app that is enabled with OAuth.
+
+    It initializes the app and web session secret keys with a randomly generated token. It defines two routes for
+    handling the callback and index pages.
+    """
+    import secrets
+
+    from flask import (Flask, redirect, render_template_string, request,
+                       session, url_for)
+
+    app = Flask(APP_NAME)
+    app.secret_key = secrets.token_urlsafe(32)
+
+    @app.route("/callback")
+    def callback():
+        """The callback route initiates consent using the OAuth client, exchanges
+        the callback parameters, and redirects the user to the index page."""
+        from databricks.sdk.oauth import Consent
+
+        consent = Consent.from_dict(oauth_client, session["consent"])
+
+        session["creds"] = consent.exchange_callback_parameters(request.args).as_dict()
+        return redirect(url_for("index"))
+
+    @app.route("/")
+    def index():
+        """The index page checks if the user has already authenticated and retrieves the user's credentials using
+        the Databricks SDK WorkspaceClient. It then renders the template with the clusters' list."""
+        if "creds" not in session:
+            consent = oauth_client.initiate_consent()
+            session["consent"] = consent.as_dict()
+            return redirect(consent.auth_url)
+
+        from databricks.sdk import WorkspaceClient
+        from databricks.sdk.oauth import SessionCredentials
+
+        credentials_provider = SessionCredentials.from_dict(oauth_client, session["creds"])
+        workspace_client = WorkspaceClient(host=oauth_client.host,
+                                           product=APP_NAME,
+                                           credentials_provider=credentials_provider,
+                                           )
+        
+        # Get OAuth access token from session credentials
+        access_token = session["creds"]["token"]["access_token"]
+
+        return call_model_serving_endpoint(access_token)
+
+    return app
+
+
+def register_custom_app(oauth_client: OAuthClient, args: argparse.Namespace) -> tuple[str, str]:
+    """Creates new Custom OAuth App in Databricks Account"""
+
+    logging.info("No OAuth custom app client/secret provided, creating new app")
+
+    import getpass
+
+    from databricks.sdk import AccountClient
+
+    account_client = AccountClient(host="https://accounts.cloud.databricks.com",
+                                   account_id=input("Databricks Account ID: "),
+                                   username=input("Username: "),
+                                   password=getpass.getpass("Password: "),
+                                   )
+
+    custom_app = account_client.custom_app_integration.create(
+        name=APP_NAME, redirect_urls=[f"http://localhost:{args.port}/callback"], confidential=True,
+        scopes=["all-apis","offline_access","user_impersonation"],
+    )
+    logging.info(f"Created new custom app: "
+                 f"--client_id {custom_app.client_id} "
+                 f"--client_secret {custom_app.client_secret}")
+
+    return custom_app.client_id, custom_app.client_secret
+
+
+def init_oauth_config(args) -> OAuthClient:
+    """Creates Databricks SDK configuration for OAuth"""
+    oauth_client = OAuthClient(host=args.host,
+                               client_id=args.client_id,
+                               client_secret=args.client_secret,
+                               redirect_url=f"http://localhost:{args.port}/callback",
+                               # All three scopes needed for model serving on Azure
+                               scopes=["all-apis", "offline_access", "user_impersonation"],
+                               )
+    if not oauth_client.client_id:
+        client_id, client_secret = register_custom_app(oauth_client, args)
+        oauth_client.client_id = client_id
+        oauth_client.client_secret = client_secret
+
+    return oauth_client
+
+
+def parse_arguments() -> argparse.Namespace:
+    """Parses arguments for this demo"""
+    parser = argparse.ArgumentParser(prog=APP_NAME, description=__doc__.strip())
+    parser.add_argument("host")
+    for flag in ["client_id", "client_secret"]:
+        parser.add_argument(f"--{flag}")
+    parser.add_argument("--port", default=5001, type=int)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(stream=sys.stdout,
+                        level=logging.INFO,
+                        format="%(asctime)s [%(name)s][%(levelname)s] %(message)s",
+                        )
+    logging.getLogger("databricks.sdk").setLevel(logging.DEBUG)
+
+    args = parse_arguments()
+    oauth_cfg = init_oauth_config(args)
+
+    # Account for bug in databricks-sdk-py oauth.py
+    # Scopes incomplete for calling model serving on Azure.
+    # PR pending
+    oauth_cfg._scopes.append("all-apis")
+    
+    app = create_flask_app(oauth_cfg, args.port)
+
+    app.run(
+        host="localhost",
+        port=args.port,
+        debug=True,
+        # to simplify this demo experience, we create OAuth Custom App for you,
+        # but it intervenes with the werkzeug reloader. So we disable it
+        use_reloader=args.client_id is not None,
+    )
